df0d7f35a741a585b22149e8e5423cacf46c2da8
Gabriel Getizo <gfsgetizo@gmail.com>
2025-03-04 19:49:03 +0800

    SCROLLABLE

--- components/shared/Calendar/Calendar.tsx
+++ components/shared/Calendar/Calendar.tsx
@@ -1,4 +1,3 @@
-
 import { ChangeEvent, FC, useCallback, useEffect, useRef, useState } from "react";
 import debounce from "lodash.debounce"
 import { useCalendar } from "@/context/CalendarProvider";
